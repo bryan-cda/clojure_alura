7b4c3442d14d108d6b71f01c96306f3b381ef6ca
Bryan Duarte <bryan.duarte@ext.valid.com>
2022-04-11 12:35:07 -0300

    Associate visit-list to patient and print report.

--- meetup/src/basic/schema/schema.clj
+++ meetup/src/basic/schema/schema.clj
@@ -6,21 +6,28 @@
                 (assoc patients id patient)
                 (throw (ex-info "Patients don't have id for " {:patient patient}))))
 
-(defn add-visit [visit-list, patient, visitor]
+(defn visit-generator [visit-list, patient, visitor]
   (if (contains? visit-list patient)
     (update visit-list patient concat visitor)
     (assoc visit-list patient visitor)))
 
+(defn visit-scheduler [daily-visit-list patient-id schedule-date-and-time]
+  (pprint "[Daily visits]")
+  (visit-generator daily-visit-list patient-id schedule-date-and-time))
+
+
+(defn visit-report [patient visits]
+  (println "[Patient]:"patient"[Visits]:" (get visits (:id patient)))
+  )
+
 (defn patient-generator []
   (let [foo {:id 1, :name "Foo"}
         bar {:id 2, :name "Bar"}
-        patients (reduce add-patient {} [foo, bar])
-        visits {}
-        visits (add-visit visits 1 ["01/01/2022"])
-        visits (add-visit visits 2 ["01/01/2022", "02/01/2022"])
-        visits (add-visit visits 1 ["03/01/2022"])
-        ]
-        (pprint patients)
-        (pprint visits)))
+        admin {:id 3, :name "admin"}
+        employee {:id 4, :name "employee"}
+        patients (reduce add-patient {} [foo, bar, admin, employee])]
+        (println (visit-report foo (visit-scheduler {} (:id foo) ["01/01/2022", "02/01/2022"])))
+        (pprint patients)))
+
 
 (patient-generator)
